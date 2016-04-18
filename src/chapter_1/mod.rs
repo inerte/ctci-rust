0f43609d1859a4ac8fe84c222d96683160a8172e
Julio Nobrega Netto <julionn@yahoo-inc.com>
2016-04-17 17:36:59 -0700

    Chapter 1, question 4 solved

--- src/chapter_1/mod.rs
+++ src/chapter_1/mod.rs
@@ -1,3 +1,4 @@
 mod question_1;
 mod question_2;
 mod question_3;
+mod question_4;
---
+++ src/chapter_1/question_4.rs
@@ -0,0 +1,55 @@
+use std::collections::HashMap;
+
+fn char_frequency(string: String) -> HashMap<char, usize> {
+    let mut frequency = HashMap::new();
+
+    for c in string.chars() {
+        if !c.is_whitespace() {
+            let counter = frequency.entry(c).or_insert(0);
+            *counter += 1;
+        }
+    }
+
+    return frequency;
+}
+
+pub fn is_permutation_of_palindrome(string: &str) -> bool {
+    let string = string.to_lowercase();
+    let char_frequency = char_frequency(string);
+
+    println!("char_frequency {:?}", char_frequency);
+
+    let mut odd_found = false;
+
+    for (_, i) in char_frequency {
+        if i % 2 == 1 {
+            if odd_found {
+                return false;
+            }
+            odd_found = true;
+        }
+    }
+
+    return true;
+}
+
+#[test]
+fn example_is_permutation_of_palindrome() {
+    assert!(is_permutation_of_palindrome("Tact Coa"));
+}
+
+#[test]
+fn one_character_is_permutation_of_palindrome() {
+    assert!(is_permutation_of_palindrome("a"));
+}
+
+#[test]
+fn a_palindrome_is_a_permutation_of_palindrome() {
+    assert!(is_permutation_of_palindrome("abba"));
+}
+
+#[test]
+#[should_panic]
+fn my_name_is_not_a_permutation_of_palindrome() {
+    assert!(is_permutation_of_palindrome("Julio"));
+}
